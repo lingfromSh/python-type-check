a3f96c9fe46d2a5ce0a5ebf4a9754698c53b3617
Stephen <lingfromsh@163.com>
2020-02-05 22:44:56 +0800

    Pack type-check[version:0.1]

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding=utf-8
+# Author:Stephen Ling
+# Email:lingfromsh@163.com
+
+from setuptools import setup, find_packages
+
+setup(
+    name="type_check",
+    version="0.1",
+    packages=find_packages(),
+    description="A decorator for type checking.",
+    author="Stephen Ling",
+    author_email="lingfromsh@163.com",
+    url="https://github.com/lingfromSh/python-type-check",
+    license="MIT License"
+)
